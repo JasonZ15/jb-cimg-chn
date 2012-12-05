57a06840c91f2a9b4bc12103859157315b818044
Jason Zhai <vines15@gmail.com>
2012-12-05 18:10:18 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,99 @@
+			</div> 	<!-- end #main-area -->
+		
+		<footer id="main_footer" class="clearfix">
+			<p id="copyright"><!--<span style="font-size:22px;">联系我们</span><br />长城会<br /><a href="mailto: info@gmic-sv.com" title="Email Us" target="_blank">info@gmic-sv.com</a><br /><a href="http://www.facebook.com/theGMIC" target="_blank">www.facebook.com/theGMIC</a><br /><a href="http://www.twitter.com/theGMIC" target="_blank">Twitter: @theGMIC</a><br />565 Clyde Ave, Suite 600, Mountain View, CA, 94043<br />-->Copyright © 全球移动互联网大会 - All rights reserved.<br />长城会主办</p>
+		</footer> <!-- end #main_footer -->
+	</div> <!-- end #container -->
+<div id="bg">
+	<img src="http://gmic.greatwallclub.com/cn/wp-content/themes/gmic/images/BG.png" alt="">
+</div>
+
+	<?php wp_footer(); ?>
+<script type="text/javascript" language="javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.carouFredSel-5.6.1-packed.js"></script>
+<script type="text/javascript" language="javascript">
+			$(function() {
+
+				//	Fuild layout example 2, centering the items
+				$("#foo3").carouFredSel({
+					auto		:  {
+						play: true,
+						delay: 2000,
+					},
+					height: 70,
+					width: '100%',
+					scroll: 2,
+				});
+				
+				//	Scrolled by user interaction
+				$('#foo2').carouFredSel({
+					pagination	: {
+						container		: "#pager2",
+						anchorBuilder	: function( nr, item ) {
+							var title = item.find( "h2.agenda_title" ).text();
+							return "<span>" + title + "</span>";
+						}
+					},
+					auto: false,
+				});
+				
+				$("#foo1").carouFredSel({
+					auto		: true,
+					auto : {pauseDuration   : 5000},
+					scroll      : {
+						duration        : 900},
+					width: '100%',
+				});
+				
+				//if (navigator.platform.match(/iP(od|hone|ad)/i)) {
+				//    $(window).scroll(function() {
+				//        $('#fixed-div').css({
+				//            position : 'absolute',
+				//            top      : ($(window).height() + $(document).scrollTop() - 110) + 'px'
+				//        });
+				//    });
+				//}
+
+				//back to top button
+				var pxShow = 300;//height on which the button will show
+				var fadeInTime = 1000;//how slow/fast you want the button to show
+				var fadeOutTime = 1000;//how slow/fast you want the button to hide
+				var scrollSpeed = 300;//how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+				jQuery(window).scroll(function(){
+					if(jQuery(window).scrollTop() >= pxShow){
+						jQuery("#backtotop").fadeIn(fadeInTime);
+					}else{
+						jQuery("#backtotop").fadeOut(fadeOutTime);
+					}
+				});
+				 
+				jQuery('#backtotop a').click(function(){
+					jQuery('html, body').animate({scrollTop:0}, scrollSpeed); 
+					return false; 
+				}); 
+
+			});
+		</script>
+		<img class="title-tab" src="<?php echo get_template_directory_uri(); ?>/images/sponsors-tab.png" />
+<div class="image_carousel">	
+	<ul id="foo3">
+	   <li style="width: 111px;"><img src="http://gmic.greatwallclub.com/cn/wp-content/themes/gmic/images/top-sponsor-tab.jpg" width="97" height="59" style="padding:0; border: none;" /></li>   
+	  <li style="width: 161px;"><a href="http://www.sina.com.cn/" target="_blank"><img src="http://gmic.greatwallclub.com/cn/wp-content/uploads/2012/09/sina.png" width="135" style="border: 1px solid #7b6596; padding: 9px 5px 10px 5px;" /></a></li>
+	  <li style="width: 161px;"><a href="http://www.qq.com/" target="_blank"><img src="http://gmic.greatwallclub.com/cn/wp-content/uploads/2012/09/tengxun.png" width="135" style="border: 1px solid #7b6596; padding: 9px 5px 10px 5px;" /></a></li>
+	  
+	  <li style="width: 161px;"><a href="http://www.91.com/" target="_blank"><img src="http://gmic.greatwallclub.com/cn/wp-content/uploads/2012/09/wang_long.png" width="135" style="border: 1px solid #7b6596; padding: 9px 5px 10px 5px;" /></a></li>
+	  
+	   <li style="width: 161px;"><a href="http://www.uc.cn/index.html" target="_blank"><img src="http://gmic.greatwallclub.com/cn/wp-content/uploads/2012/09/ucweb.jpg" width="135" style="border: 1px solid #7b6596; padding: 9px 5px 10px 5px;" /></a></li>
+	   
+	   <li style="width: 111px;"><img src="http://gmic.greatwallclub.com/cn/wp-content/themes/gmic/images/gold-sponsor-tab.jpg" width="97" height="59" style="padding:0; border: none;" /></li>
+	  <li style="width: 139px;"><a href="http://www.domob.cn/" target="_blank"><img src="http://gmic.greatwallclub.com/cn/wp-content/uploads/2012/09/domob2.png" height="57" 
+	  style="border: 1px solid #eba706; padding: 0;" /></a></li>
+	  
+	</ul>
+	<div class="clearfix"></div>
+	<div class="pagination" id="foo3_pag"></div>
+</div>
+<div id="backtotop">
+   <a href="#">&uarr; TOP</a>
+</div>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,122 @@
+<?php get_header(); ?>
+<?php get_template_part('includes/breadcrumbs', 'page'); ?>
+<?php if ( 'on' == et_get_option('flexible_featured', 'on') && is_home() ) get_template_part( 'includes/featured', 'home' ); ?>
+<!--<div id="speaker_slider" class="image_carousel">
+	<p>GMIC硅谷大会演讲嘉宾</p>
+	<div id="foo1">
+		<div id="speaker1"><a href="http://www.gmic-sv.com/kevin-chou/" target="_blank"></a><span>Kevin Chou</span><span>CEO, Kabam, Inc.</span></div>
+		<div id="speaker2"><a href="http://www.gmic-sv.com/tim-draper/" target="_blank"></a><span>Tim Draper</span><span>Managing Director, DFJ</span></div>
+		<div id="speaker3"><a href="http://www.gmic-sv.com/lei-jun-2/" target="_blank"></a><span>Lei Jun</span><span>CEO, Xiaomi</span></div>
+		<div id="speaker4"><a href="http://www.gmic-sv.com/aaron-levie/" target="_blank"></a><span>Aaron Levie</span><span>CEO, Box</span></div>
+		<div id="speaker5"><a href="http://www.gmic-sv.com/phil-libin/" target="_blank"></a><span>Phil Libin</span><span>CEO, Evernote</span></div>
+		<div id="speaker6"><a href="http://www.gmic-sv.com/niccolo-de-masi/" target="_blank"></a><span>Niccolo de Masi</span><span>CEO, Glu Mobile</span></div>
+		<div id="speaker7"><a href="http://www.gmic-sv.com/john-riccitiello/" target="_blank"></a><span>John Riccitiello</span><span>CEO, Electronic Arts (EA)</span></div>
+		<div id="speaker8"><a href="http://www.gmic-sv.com/mihir-shah/" target="_blank"></a><span>Mihir Shah</span><span>CEO, Tapjoy</span></div>
+		<div id="speaker9"><a href="http://www.gmic-sv.com/vaughan-smith/" target="_blank"></a><span>Vaughan Smith</span><span>VP, Facebook</span></div>
+		<div id="speaker10"><a href="http://www.gmic-sv.com/tayloe-stansbury/" target="_blank"></a><span>Tayloe Stansbury</span><span>CTO, Intuit</span></div>
+		<div id="speaker11"><a href="http://www.gmic-sv.com/yu-yongfu-2/" target="_blank"></a><span>Yu Yongfu</span><span>CEO, UC Web</span></div>
+	</div>
+	<div class="clearfix"></div>
+</div>-->
+
+<?php if ( 'on' == et_get_option('flexible_quote','on') ) { ?>
+	<div id="quote">
+		<h2><?php echo et_get_option('flexible_quote_heading'); ?></h2>
+		<p><?php echo esc_html( et_get_option('flexible_quote_text') ); ?></p>
+	</div> <!-- end #quote -->
+<?php } ?>
+
+<?php if ( 'on' == et_get_option('flexible_display_recentwork_section','on') ) { ?>
+	<section id="portfolio" class="clearfix">
+		<h1 class="section-title"><?php esc_html_e( 'Design', 'Flexible' ); ?></h1>
+		<span class="section-tagline"><?php esc_html_e( 'Recent work', 'Flexible' ); ?></span>
+		
+		<a href="<?php echo esc_url( et_get_option( 'flexible_more_work_url', '#' ) ); ?>" class="more"><?php esc_html_e( 'More work &raquo;', 'Flexible' ); ?></a>
+		
+		<?php
+			$portfolio_args = array(
+				'post_type' => 'project',
+				'showposts' => (int) et_get_option('flexible_homepage_numposts_projects'),
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'project_category',
+						'field' => 'id',
+						'terms' => (array) et_get_option('flexible_homepage_exlcats_projects'),
+						'operator' => 'NOT IN'
+					)
+				)
+			);
+		
+			$categories = get_terms( 'project_category', array( 'exclude' => array( et_get_option('flexible_homepage_exlcats_projects') ) ) );
+			if ( $categories ){
+				echo '<ul id="et_portfolio_sort_links">';
+					echo '<li class="active">' . '<a href="#" data-categories_id="all">' . __( 'All', 'Flexible' ) . '</a>' . '</li>';
+					foreach ( $categories as $category ){
+						echo '<li>' . '<a href="#" data-categories_id="' . esc_attr( 'project_cat_' . $category->term_taxonomy_id ) . '">' . esc_html( $category->name ) . '</a>' . '</li>';
+					}
+				echo '</ul>';
+			}
+		?>
+		
+		<div id="portfolio-grid" class="clearfix">
+		<?php
+			$portfolio_query = new WP_Query( apply_filters( 'et_home_portfolio_args', $portfolio_args ) );
+			while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post();			
+				get_template_part( 'includes/entry', 'portfolio' );
+			endwhile; 
+			wp_reset_postdata(); 
+		?>
+		</div> <!-- end #portfolio-grid -->
+	</section> <!-- end #portfolio -->
+<?php } ?>
+
+<?php if ( 'on' == et_get_option('flexible_display_fromblog_section','on') && ( 'false' == et_get_option('flexible_blog_style','false') ) ) { ?>
+	<section id="blog" class="clearfix">
+		<h1 class="section-title"><a href="http://gmic.greatwallclub.com/cn/category/recent-news/" target="_blank"><?php esc_html_e( '最新消息', 'Flexible' ); ?></a></h1>
+		<!--<span class="section-tagline"><?php esc_html_e( '最新消息', 'Flexible' ); ?></span>-->
+		
+		<a href="<?php echo esc_url( et_get_option( 'flexible_more_posts_url', '#' ) ); ?>" class="more"><?php esc_html_e( '阅读更多 &raquo;', 'Flexible' ); ?></a>
+		
+		<div id="blog-grid">
+			<?php $i = 0; ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php 
+					$i++;
+					$last_class = ( $i % 3 == 0 ) ? ' last' : '';
+				?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class('blog-item' . $last_class); ?>>
+					<span class="date"><?php echo get_the_time( 'M' ); ?><strong><?php echo get_the_time( 'd' ); ?></strong></span>
+
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<p class="meta-info"><?php printf( __('Posted on %1$s by %2$s', 'Flexible'), get_the_time( apply_filters( 'et_home_post_date_format', 'M j' ) ), et_get_the_author_posts_link() ); ?></p>
+					<p><?php the_excerpt(); ?></p>
+					
+					<!--<p><?php truncate_post(375); ?></p>-->  
+				</article> <!-- end .blog-item -->
+			<?php endwhile; else : ?>
+				<article id="post-0" class="post no-results not-found">
+					<h2 class="entry-title"><?php _e( 'Nothing Found', 'Flexible' ); ?></h1>
+				</article><!-- end #post-0 -->
+			<?php endif; ?>
+		</div> <!-- end #blog-grid -->
+	</section> <!-- end #blog -->
+<?php } ?>
+
+<?php if ( 'on' == et_get_option('flexible_blog_style') ) { ?>
+	<div id="content-area" class="clearfix">
+		<div id="left-area">		
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php get_template_part('includes/entry', 'index'); ?>
+			<?php
+			endwhile;
+				if (function_exists('wp_pagenavi')) { wp_pagenavi(); }
+				else { get_template_part('includes/navigation','entry'); }
+			else:
+				get_template_part('includes/no-results','entry');
+			endif; ?>
+		</div> 	<!-- end #left-area -->
+		<?php get_sidebar(); ?>
+	</div> 	<!-- end #content-area -->
+<?php } ?>
+	
+<?php get_footer(); ?>
