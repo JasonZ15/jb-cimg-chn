1aba1865e3e90626f271eba6f53937a02db563a9
Darren <darren@greatwallclub.com>
2012-12-26 19:13:07 +0800

    "recently news url" update
    probably better to use relative url instead of absolute url in the
    future?

--- home.php
+++ home.php
@@ -72,7 +72,7 @@
 
 <?php if ( 'on' == et_get_option('flexible_display_fromblog_section','on') && ( 'false' == et_get_option('flexible_blog_style','false') ) ) { ?>
 	<section id="blog" class="clearfix">
-		<h1 class="section-title"><a href="http://gmic.greatwallclub.com/cn/category/recent-news/" target="_blank"><?php esc_html_e( '最新消息', 'Flexible' ); ?></a></h1>
+		<h1 class="section-title"><a href="/cn/category/recent-news/" target="_blank"><?php esc_html_e( '最新消息', 'Flexible' ); ?></a></h1>
 		<!--<span class="section-tagline"><?php esc_html_e( '最新消息', 'Flexible' ); ?></span>-->
 		
 		<a href="<?php echo esc_url( et_get_option( 'flexible_more_posts_url', '#' ) ); ?>" class="more"><?php esc_html_e( '阅读更多 &raquo;', 'Flexible' ); ?></a>
