8ef4533da4d5f1c8abeb7cc75129d54c305cc32c
Darren <darren@greatwallclub.com>
2013-04-10 22:07:39 +0800

    sidebar changes

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,134 @@
 <?php if ( is_active_sidebar( 'sidebar' ) ){ ?>
 	<div id="sidebar">
-		<?php dynamic_sidebar( 'sidebar' ); ?>
+
+<script src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/SlideTrans.js"></script>
+<style>
+table{ padding:0; border:0;}
+table td{ padding:0; border:0;}
+img{ border:0;}
+.box-right{ padding-left:40px; padding-left:20px; width:227px; float:left;}
+#sidebar{ width:310px; float:left;}
+.down-app{ border-bottom:1px solid #f4f4f4; height:120px;}
+.down-app h2{ font-family:"微软雅黑"; font-size:14px; color:#848484;width:auto; height:35px; background:#f4f4f4 url(http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_03.jpg) no-repeat 35px 6px; margin:auto; padding-left:63px; line-height:35px; font-weight:normal; margin-left:}
+.down-app ul{}
+.down-app ul li{ list-style:none; margin:auto; width:50px; height:50px; float:left; padding-left:8px; padding-right:8px;}
+.right-list{}
+.right-list h2{ background:url(http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_14.jpg) no-repeat 0 30px; height:35px; font-family:"微软雅黑"; font-size:14px; line-height:38px; color:#0455a0; width:190px; float:left; font-weight:normal; padding:0; margin:0; margin-bottom:5px; margin-top:3px;}
+.right-list ul{ padding:0 0 0 14px; margin:0;}
+.right-list ul li{ float:left; font-size:12px; font-family:"宋体"; color:#464646; line-height:22px; width:150px;}
+.right-list ul li a{ text-decoration:none; color:#464646;}
+.right-pic-list{}
+.right-pic-list h2{ background:url(http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_14.jpg) no-repeat 0 30px; height:35px; font-family:"微软雅黑"; font-size:14px; line-height:38px; color:#0455a0; width:190px; float:left; font-weight:normal; padding:0; margin:0; margin-bottom:5px; margin-top:8px;}
+.right-pic-list ul{ padding:0px 0 0 5px; margin:0;}
+.right-pic-list ul li{ float:left; text-align:center; padding:16px 0 0px 0; list-style:none;width:183px;}
+.right-pic-list ul li a{ text-decoration:none; color:#464646;}
+.container, .container img{ width:183px; height:67px;}
+.container img{border:0;vertical-align:top; width:183px;}
+.right-jiaru{ background:#f4f4f4 url(http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_36.jpg) no-repeat; font-size:12px; color:#295595; height:24px; float:left; line-height:24px; width:82px; padding-left:105px; margin-top:15px; display:inline;}
+.right-jiaru a{ color:#295595; text-decoration:none;}
+.img-183 img{ width:183px; border:0;}
+.container ul{ margin:0; padding:0;}
+.container ul li{ float:left; border:0; list-style:none;}
+</style>
+
+<script type="text/javascript">
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+<div class="box-right">
+<!-- <div class="down-app">
+    	<h2>长城会APP下载</h2>
+        <ul><li><a href="#"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_07.jpg" width="50" height="50" id="Image1" onmouseover="MM_swapImage('Image1','','http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_07-.jpg',1)" onmouseout="MM_swapImgRestore()" /></a></li>
+        <li><a href="#"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_07-03.jpg" width="50" height="50" id="Image2" onmouseover="MM_swapImage('Image2','','http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_07-02-.jpg',1)" onmouseout="MM_swapImgRestore()" /></a></li>
+        </ul>
+    </div>-->
+    <div class="right-list">
+    <h2>快速通道</h2>
+    <ul>
+    	<li><a href="http://beijing.thegmic.com/cn/gmic-advisors/" target="_blank">大会顾问团</a></li>
+        <li><a href="http://beijing.thegmic.com/cn/sponsorship/" target="_blank">赞助大会</a></li>
+        <li><a href="http://beijing.thegmic.com/cn/partnership/" target="_blank">商务合作</a></li>        
+        <li><a href="http://beijing.thegmic.com/cn/recommend-a-speaker/" target="_blank">演讲申请</a></li>
+         <li><a href="http://beijing.thegmic.com/cn/媒体通道/" target="_blank">媒体通道</a></li>
+        <li><a href="http://beijing.thegmic.com/cn/volunteers/" target="_blank">大会志愿者报名</a></li>
+        <li><a href="http://beijing.thegmic.com/cn/receive-updates/" target="_blank">订阅最新消息</a></li>
+        <li style="padding-top:3px;"><a href="http://share.gwc.net/sites/default/files/GMIC-Beijing-2013-Brochure-cn.pdf"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_18.jpg" width="150" height="14" /></a></li>
+        <li><a href="http://share.gwc.net/sites/default/files/GMIC-Beijing-2013-Brochure-en.pdf"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_18-02.jpg" width="150" height="14" /></a></li>
+    </ul>
+    </div>
+    <div class="right-list">
+    <h2>往届回顾</h2>
+    <ul>
+    	<li><a href="http://gmic2012.gwc.net/cn" target="_blank"><font color="#FF0000">2012</font>年GMIC大会</a></li>
+        <li><a href="http://gmic2011.gwc.net/cn" target="_blank"><font color="#640215">2011</font>年GMIC大会</a></li>
+        <li><a href="http://www.gwc.net/GMIC2010" target="_blank"><font color="#008899">2010</font>年GMIC大会</a></li>
+        <li><a href="http://www.gwc.net/GMIC2009" target="_blank"><font color="#7c35ae">2009</font>年GMIC大会</a></li>
+        <li><a href="http://sv.thegmic.com/" target="_blank"><font color="#19aef4">GMIC SV</font>硅谷大会</a></li>
+    </ul>
+    </div>
+  <div class="right-pic-list">
+    <h2>指导单位</h2>
+    <ul>
+    	<!--<li><a href="http://www.zgc.gov.cn/" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_24.jpg" width="183" height="49" /></a></li>-->
+        <li><a href="http://www.isc.org.cn/" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_26.jpg" width="183" height="66" /></a></li>
+       <!-- <li><a href="http://www.bjhd.gov.cn/" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/GMIC-2013-page-v1_29.jpg" width="183" height="36" /></a></li>-->
+    </ul>
+    </div>
+    
+      <div class="right-pic-list">
+    <h2>主办单位</h2>
+    <ul>
+    	<li><a href="http://www.gwc.net" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/gwc-logo.jpg" width="183" height="36" /></a></li>
+
+    </ul>
+    </div>
+    
+      <!--<div class="right-pic-list">
+    <h2>赞助单位</h2>
+     <ul>
+    	<div class="container" id="idContainer" style="float:left; padding-top:10px; width:183px; overflow:hidden; height:80px;">
+        <div id="idSlider">
+        <ul>
+     <li><a href="http://www.sina.com.cn" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/sina-logo.jpg" width="183" height="67"/></a></li>
+      <li><a href="http://www.qq.com" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/tencent-logo.jpg" width="183" height="67"/></a></li>
+      <li><a href="http://www.91.com/" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/91-logo.jpg" width="183" height="67"/></a></li>
+      <li><a href="http://www.uc.cn" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/uc-logo.jpg" width="183" height="67"/></a>
+    <li><a href="http://www.xiaomi.com" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/xiaomi-logo.jpg" width="183" height="67"/></a></li>
+       <li><a href="http://china.inmobi.com/" target="_blank"><img src="http://beijing.thegmic.com/cn/wp-content/themes/gmic/images/2013-right/inmobi-logo.jpg" width="183" height="67"/></a></li>
+</ul>
+        </div> 
+
+</div>
+<script>
+new SlideTrans("idContainer", "idSlider", 6, { Vertical: false }).Run();
+</script>
+<div class="right-jiaru"><a href="http://beijing.thegmic.com/cn/sponsorship/" target="_blank">成为赞助商>></a></div>
+    </ul>
+    </div> -->
+    
+    
+
+</div>
+
 	</div> <!-- end #sidebar -->
 <?php } ?>
